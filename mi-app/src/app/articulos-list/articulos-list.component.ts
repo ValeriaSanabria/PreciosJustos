c75478fba8b8ab6a52a63e96b363da9e267b6471
ValeriaSanabria <valeriasanabria@tdf.edu.ar>
2022-12-08 10:19:15 -0300

    filtro

--- mi-app/src/app/articulos-list/articulos-list.component.ts
+++ mi-app/src/app/articulos-list/articulos-list.component.ts
@@ -8,6 +8,7 @@ import {
 import {
   ActivatedRoute
 } from '@angular/router';
+import { Location } from '@angular/common';
 
 export interface Articulo {
   ean: number,
@@ -21,25 +22,60 @@ export interface Articulo {
   styleUrls: ['./articulos-list.component.css']
 })
 export class ArticulosListComponent implements OnInit {
-  articulos: Articulo[] = []
-  provinciaSlctd: string = "catamarca";
+  // lista completa de productos - backup
+  articulos: Articulo[] = [];
+  // lista de productos filtrados, que le muestro usuarios
+  articulosFiltrados: Articulo[] = [];
+  provinciaSlctd: string = 'tierra-del-fuego';
+  filtroTexto: string = "";
+  filtroPrecio: number = 0;
+  precioMaximo: number = 1000;
 
-  constructor(productosSrv: ArticulosService,
-    actRoute: ActivatedRoute) {
-    const {
-      nombreProvincia
-    } = actRoute.snapshot.params;
+  constructor(articulosSrv: ArticulosService,actRoute: ActivatedRoute,private location: Location) {
+    const { nombreProvincia } = actRoute.snapshot.params;
 
     this.provinciaSlctd = nombreProvincia;
-    
-    productosSrv.getArticulos(nombreProvincia).subscribe((data: any) => {
+
+    articulosSrv.getArticulos(nombreProvincia).subscribe((data: any) => {
       this.articulos = data;
+      this.articulosFiltrados = data;
+      // actualizar la variable que contiene el precio maximo de los productos
+      this.precioMaximo = Math.max(...this.articulos.map((articulo) => articulo.precio)) + 1;
+      this.filtroPrecio = this.precioMaximo;
     });
+  }
+  goBack() {
+    this.location.back();
+  }
+  onFiltroTextoChange(parametroIngresado: any) {
+    this.articulosFiltrados = this.articulos.filter((articulo) => {
+      if (this.filtroPrecio != 0){
+        // esto significa que el usuario ingreso filtro
+        // tengo que filtrar por nombre y codigo y ademas por precio
+        return (articulo.nombre.toLowerCase().includes(parametroIngresado.toLowerCase())
+            || (articulo.ean + '').includes(parametroIngresado)) && articulo.precio <= this.filtroPrecio;
+      }
+      else{
+        // solo filtro por nombre y por codigo
+        return articulo.nombre.toLowerCase().includes(parametroIngresado.toLowerCase())
+          || (articulo.ean + '').includes(parametroIngresado);
+      }
+    });
+  }
 
-
-    // this.provinciaSlctd = id;
+  onFiltroPrecioChange(parametroIngresado: any) {
+    this.articulosFiltrados = this.articulos.filter((articulo) => {
+      if (this.filtroTexto != ""){
+        return articulo.precio <= parametroIngresado && (
+          articulo.nombre.toLowerCase().includes(this.filtroTexto.toLowerCase())
+            || (articulo.ean + '').includes(this.filtroTexto)
+        )
+      }
+      else{
+        return articulo.precio <= parametroIngresado;
+      }
+    })
   }
 
   ngOnInit(): void {}
-
 }
